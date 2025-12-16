f879ebd264ac0cd632f38153585f5275fa66a1b5
NIRIF-odoo <nirif@odoo.com>
2025-12-16 14:22:10 +0400

    Up to Chapter 9

---
+++ estate/models/estate_property.py
@@ -0,0 +1,77 @@
+from odoo import api,models,fields
+from odoo.exceptions import UserError
+from dateutil.relativedelta import relativedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Properties"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default = fields.Datetime.today() + relativedelta(months=3),string="Available From")
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    has_garage = fields.Boolean()
+    has_garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('north','North'), ('south','South'),('east','East'),('west','West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[('new','New'),('offer_received','Offer Received'),('offer_accepted','Offer Accepted'),('sold','Sold'),
+                   ('cancelled','Cancelled')], default='new', required=True, copy=False
+    )
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    buyer_id = fields.Many2one('res.partner',copy=False)
+    sales_person = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag")
+    offer_ids = fields.One2many("estate.property.offer","property_id")
+    total_area = fields.Integer(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price",string="Best Offer")
+
+
+
+
+    @api.depends("living_area","garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped("price"))
+            else:
+                record.best_price = 0.0
+
+    @api.onchange("has_garden")
+    def _onchange_garden(self):
+        if self.has_garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = False
+            self.garden_orientation = False
+
+
+    def sell_property(self):
+        for record in self:
+            if record.state == 'cancelled':
+                raise UserError("Cancelled properties cannot be sold")
+            else:
+                record.state = 'sold'
+    
+
+    def cancel_property(self):
+        for record in self:
+            if record.state == 'sold':
+                raise UserError("Sold properties cannot be cancelled")
+            else:
+                record.state = 'cancelled'
---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,43 @@
+from odoo import api,models,fields
+from odoo.exceptions import UserError
+from dateutil.relativedelta import relativedelta
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
+
+    price = fields.Float()
+    status = fields.Selection(selection=[('accepted','Accepted'),('refused','Refused')], copy=False)
+    partner_id = fields.Many2one('res.partner',required=True)
+    property_id = fields.Many2one('estate.property',required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+
+
+    @api.depends("create_date","validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = create_date + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            create_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - create_date).days
+
+
+    def accept_property(self):
+        for record in self:
+            record.status = 'accepted'
+            record.property_id.write({
+                "buyer_id" : record.partner_id.id,
+                "selling_price" : record.price,                      
+              })
+    
+    def refuse_property(self):
+        for record in self:
+            if record.status == 'accepted':
+                raise UserError("Accepted properties cannot be rejected")
+            else:
+                record.status = 'refused'
